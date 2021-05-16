fed6fa1a38da6799941a602369e633969235f3e4
KevinLohJunYong <e0439232@u.nus.edu>
2021-05-16 12:17:59 +0800

    created a grid

--- src/index.js
+++ src/index.js
@@ -1,15 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var player = 'X';
+
+var isEnd = false;
+
+class Square extends React.Component {
+  constructor(props) {
+     super(props);
+     this.state = {
+       value: null,
+     }
+  }
+  render() {
+    return (
+      <div>
+        <button 
+           style={{width:"50px",height:"50px",backgroundColor:"white",fontSize:"x-large"}}
+           onClick={() => this.setState({value:'X'})}>
+           {this.state.value}
+        </button>
+      </div>
+    );
+  };
+}
+
+let renderSquare = (_squareId) => {
+   return (
+     <Square />
+   );
+};
+
+let renderRow = (_squareId) => {
+  return (
+    <div style={{display:"flex",flexDirection:"row"}}>
+         {renderSquare(_squareId)}
+         {renderSquare(_squareId+1)}
+         {renderSquare(_squareId+2)}
+    </div>
+  );
+};
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+         {renderRow(1)}
+         {renderRow(4)}
+         {renderRow(7)}
+      </div>
+  );
+  };
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <p>
+           It is {player}'s turn
+         </p>
+        <Board />
+      </div>
+    );
+  };
+}
+
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
