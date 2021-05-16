c9118e6518dbdca5cdc775e56d10a90549f86b49
KevinLohJunYong <e0439232@u.nus.edu>
2021-05-16 20:44:19 +0800

    allowed clicking of squares to display the player

--- src/index.js
+++ src/index.js
@@ -3,33 +3,65 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+var player = 'X';
+
 class Square extends React.Component {
   render() {
     return (
-       <button style={{backgroundColor:"white",width:"50px",height:"50px"}} onClick={this.props.onClick}>
+       <button style={{backgroundColor:"white",width:"50px",height:"50px",fontSize:"x-large"}} 
+        onClick={this.props.onClick}>
          {this.props.value}
        </button>
     );
   };
 }
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+  handleClick(id) {
+    const squares = this.state.squares.slice();
+    squares[id] = player;
+    this.setState({squares: squares});
+  }
   render() {
     return (
        <div>
          <div style={{display:"flex",flexDirection:"row"}}>
-             <Square></Square>
-             <Square></Square>
-             <Square></Square>
+             <Square
+                onClick={()=>this.handleClick(0)}
+                value={this.state.squares[0]}></Square>
+             <Square
+                onClick={()=>this.handleClick(1)}
+                value={this.state.squares[1]}></Square>
+             <Square
+                onClick={()=>this.handleClick(2)}
+                value={this.state.squares[2]}></Square>
          </div>
          <div style={{display:"flex",flexDirection:"row"}}>
-             <Square></Square>
-             <Square></Square>
-             <Square></Square>
+            <Square
+                onClick={()=>this.handleClick(3)}
+                value={this.state.squares[3]}></Square>
+             <Square
+                onClick={()=>this.handleClick(4)}
+                value={this.state.squares[4]}></Square>
+             <Square
+                onClick={()=>this.handleClick(5)}
+                value={this.state.squares[5]}></Square>
          </div>
          <div style={{display:"flex",flexDirection:"row"}}>
-             <Square></Square>
-             <Square></Square>
-             <Square></Square>
+             <Square
+                onClick={()=>this.handleClick(6)}
+                value={this.state.squares[6]}></Square>
+             <Square
+                onClick={()=>this.handleClick(7)}
+                value={this.state.squares[7]}></Square>
+             <Square
+                onClick={()=>this.handleClick(8)}
+                value={this.state.squares[8]}></Square>
          </div>
        </div>
     );
